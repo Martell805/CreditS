c8138f7886fcf06ee9c791de6cd051aa762f0e90
Martell <vovasamb@yandex.ru>
2023-04-30 14:26:45 +0300

    Release script updated

--- release.cmd
+++ release.cmd
@@ -1,3 +1,4 @@
 cmd /C mvnw clean package -Dmaven.test.skip -f pom.xml
+cmd /C docker image rm martell805/credits:1.0
 cmd /C docker build -t martell805/credits:1.0 .
 cmd /C docker push martell805/credits:1.0
